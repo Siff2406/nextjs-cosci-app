4452db6a0fc4e453683567bbe6b0ff0cd0c3ea47
Siff2406 <Hasifsalika1@gmail.com>
2025-05-27 11:35:50 +0700

    add event button click

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,13 +1,19 @@
+'use client'
+
 export default function AppWelcome() {
     const title = 'Welcome to COSCI jaaaa';
     const nowYear = <p>2025</p>;
     const isShow = true;
+    const handleClick = () => {
+        alert('Please contact our Admin!');
+    }
     return (
         <>
         <p>{title.toUpperCase()}</p>
         {nowYear}
         {isShow && <p>Date: 10/10/1998</p>}
         {isShow ? <p>Hello Next.js</p> : <p>Hello JavaScript</p>}
+        <button className="bg-blue-300 p-3 m-3 text-white rounded-lg" onClick={handleClick}>Click!</button>
         </>
     );
 }
