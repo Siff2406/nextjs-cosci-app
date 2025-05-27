fe1f131cded8cd43686cef677e6c387eb95c83cc
Siff2406 <Hasifsalika1@gmail.com>
2025-05-27 13:06:05 +0700

    props

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,19 +1,23 @@
 'use client'
 
-export default function AppWelcome() {
+type AppWelcomeProps = {
+    headTitle?: string;
+    isShow: boolean;
+}
+export default function AppWelcome({headTitle, isShow}: AppWelcomeProps) {
     const title = 'Welcome to COSCI jaaaa';
     const nowYear = <p>2025</p>;
-    const isShow = true;
     const handleClick = () => {
         alert('Please contact our Admin!');
     }
     return (
         <>
-        <p>{title.toUpperCase()}</p>
-        {nowYear}
-        {isShow && <p>Date: 10/10/1998</p>}
-        {isShow ? <p>Hello Next.js</p> : <p>Hello JavaScript</p>}
-        <button className="bg-blue-300 p-3 m-3 text-white rounded-lg" onClick={handleClick}>Click!</button>
+            <h1>{headTitle}</h1>
+            <p>{title.toUpperCase()}</p>
+            {nowYear}
+            {isShow && <p>Date: 10/10/1998</p>}
+            {isShow ? <p>Hello Next.js</p> : <p>Hello JavaScript</p>}
+            <button className="bg-blue-300 p-3 m-3 text-white rounded-lg" onClick={handleClick}>Click!</button>
         </>
     );
 }
