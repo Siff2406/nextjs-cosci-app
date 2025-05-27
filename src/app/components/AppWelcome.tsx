fd2797699445aaacc40fa503bfeca0d61e121c0a
Siff2406 <Hasifsalika1@gmail.com>
2025-05-27 11:17:08 +0700

    If-else conditions

--- src/app/components/AppWelcome.tsx
+++ src/app/components/AppWelcome.tsx
@@ -1,7 +1,13 @@
 export default function AppWelcome() {
+    const title = 'Welcome to COSCI jaaaa';
+    const nowYear = <p>2025</p>;
+    const isShow = true;
     return (
         <>
-        <p>Welcome to COSCI Jaaaaa</p>
+        <p>{title.toUpperCase()}</p>
+        {nowYear}
+        {isShow && <p>Date: 10/10/1998</p>}
+        {isShow ? <p>Hello Next.js</p> : <p>Hello JavaScript</p>}
         </>
     );
 }
