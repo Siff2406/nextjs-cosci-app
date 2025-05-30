9f7c36ca6c03eecd5028680e4ebfe25efb3b7619
Siff2406 <Hasifsalika1@gmail.com>
2025-05-30 09:48:28 +0700

    edit login and sing up page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,8 +1,107 @@
-export default function Signup() {
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().email("กรุณากรอกอีเมลที่ถูกต้อง"),
+  password: z.string().min(6, "รหัสผ่านต้องมีอย่างน้อย 6 ตัวอักษร"),
+});
+
+const Signup01Page = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
+
   return (
-    <main>
-    <hr />
-    <h1 className="bg-red-400">ยังไม่เปิดการลงทะเบียนจร้าาาาาา</h1>
-    </main>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-xs w-full flex flex-col items-center">
+        <p className="mt-4 text-xl font-bold tracking-tight">
+          Sign up to COSCI online store
+        </p>
+
+        <div className="my-7 w-full flex items-center justify-center overflow-hidden">
+        </div>
+
+        <Form {...form}>
+          <form
+            className="w-full space-y-4"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="Email"
+                      className="w-full"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Password"
+                      className="w-full"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="mt-4 w-full">
+              Continue with Email
+            </Button>
+          </form>
+        </Form>
+
+        <div className="mt-5 space-y-5">
+          <p className="text-sm text-center">
+            You already have an account in COSCI online store
+            <Link href="/login" className="ml-1 underline text-muted-foreground">
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default Signup01Page;
+
